57a0aef5a11b7345bbea59530f032ed422bd444b
Maheshwaran G <maheshwarang16@gmail.com>
2025-06-24 11:54:17 +0530

    code encrypted

--- backend/utils.py
+++ backend/utils.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from twilio.rest import Client
 
 # Twilio Credentials
-TWILIO_ACCOUNT_SID = 'AC6544420d42d8e8cf1f84b6352323e5bd'
-TWILIO_AUTH_TOKEN = 'e4b0cf64666df8079fde04bbb68be6ef'
-TWILIO_PHONE_NUMBER = '+16408671356'
+TWILIO_ACCOUNT_SID = 'Your_Twilio_Account_SID'
+TWILIO_AUTH_TOKEN = 'Your_Twilio_Auth_Token'
+TWILIO_PHONE_NUMBER = 'Your_Twilio_Phone_Number'
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 def gen_complaint_id():
